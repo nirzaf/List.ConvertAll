9548feb6ec0ffacfcf2bf2b87fada1fd63e1e121
Farook M Fazrin <nirzaf1@hotmail.com>
2022-01-29 11:59:42 +0530

    Add project files.

---
+++ List.ConvertAll/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using static System.Console;
+
+List<int> intCollection = new() {1,2,3,4,5,6,7,8,9};
+
+List<string> convertAll = intCollection.ConvertAll(c => c.ToString());
+
+foreach (var sVal in convertAll)
+{
+    WriteLine(sVal);
+}
