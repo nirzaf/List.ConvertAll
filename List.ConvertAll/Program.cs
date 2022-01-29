25cb94ad0dfe5f7cd03737dd1466c6362a3f3412
Farook M Fazrin <nirzaf1@hotmail.com>
2022-01-29 12:03:19 +0530

    //Shortest way to iterate through List and Write into console

--- List.ConvertAll/Program.cs
+++ List.ConvertAll/Program.cs
@@ -4,7 +4,5 @@ List<int> intCollection = new() {1,2,3,4,5,6,7,8,9};
 
 List<string> convertAll = intCollection.ConvertAll(c => c.ToString());
 
-foreach (var sVal in convertAll)
-{
-    WriteLine(sVal);
-}
+//Shortest way to iterate through List and Write into console
+convertAll.ForEach(WriteLine);
